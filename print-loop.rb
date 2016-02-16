fe9c2066f2cbca79d74194b1f72de078014c9e80
Mubarak Imam <mubarakadeimam@yahoo.com>
2016-02-16 14:36:10 +0100

    4th version

--- print-loop.rb
+++ print-loop.rb
@@ -1,9 +1,10 @@
 def string_loop (name)
   count = 1
-  name.each_char {
-    |x| puts "#{count} #{x}"
+  names = name.split("")
+  while count <= names.length
+    puts "#{count} #{names[count-1]}"
     count += 1
-  }
+  end
 end
 print "Please, enter your name: "
 string_loop(gets.chomp().upcase)
